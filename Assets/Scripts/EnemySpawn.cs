5edf9feeffd974dfd38f6b793530f3ee0de9b814
ivevkz <ivevkz@gmail.com>
2020-07-22 16:08:51 +0600

    EnemySpawn

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _templete;
+    [SerializeField] private float _time;
+    [SerializeField] private float _countTemplete;
+    
+    private Transform[] _points;
+    private float _runningTime, _counterTemplete;
+    private int _index;
+
+    void Start()
+    {
+        _points = GetComponentsInChildren<Transform>();        
+    }
+
+    void Update()
+    {      
+        if (_countTemplete >= _counterTemplete)
+        {            
+            _runningTime += Time.deltaTime;
+            if (_time <= _runningTime)
+            {
+                _index = Random.Range(0, _points.Length - 1);
+                GameObject gameObject = Instantiate(_templete, _points[_index].position, Quaternion.Euler(
+                    Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                gameObject.transform.localScale = new Vector3(Random.Range(1, 4), Random.Range(1, 4), Random.Range(1, 4));
+
+                _runningTime = 0;
+                _counterTemplete++;
+            }
+        }    
+    }
+}
